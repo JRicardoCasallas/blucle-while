79f4d2e8152c34c0e4d844ed8c7d36c600a5879a
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-16 22:03:21 -0500

    bucles

--- while.js
+++ while.js
@@ -1,8 +1,41 @@
-var i = 1
+// var i = 1
 
-while(i < 101){
+// while(i < 101){
 
-    console.log(i)
+//     console.log(i)
 
+//     i++
+// }
+
+
+// var color = "amarillo"
+
+
+// switch (color) {
+//     case"rojo":
+//     console.log("el color es rojo")
+//     case"Azul":
+//     console.log("el color es amarillo")
+//     case"amarillo":
+//     console.log("el color es rojo")
+
+// }
+
+// var i = 12
+
+// do{
+//     console.log(i)
+//     i++
+// }while(i < 11)
+
+
+var i = 0
+
+
+
+while (i < 11) {
     i++
+    if(i === 5) continue
+    console.log(i)
+    
 }
